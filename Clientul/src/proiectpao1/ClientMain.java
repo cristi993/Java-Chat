44a21fae46176848e09980eb7322c663dce66556
cristi993 <kingccc.cristi@gmail.com>
2014-03-14 18:24:37 +0200

    Initial commit

---
+++ Clientul/src/proiectpao1/ClientMain.java
@@ -0,0 +1,8 @@
+package proiectpao1;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        new clientGUI().setVisible(true);
+    }
+    
+}
---
+++ Serverul/src/serverul/ServerulMain.java
@@ -0,0 +1,9 @@
+package serverul;
+
+public class ServerulMain {
+
+    public static void main(String[] args) {
+        new serverGUI().setVisible(true);
+    }
+    
+}
